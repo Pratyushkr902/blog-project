693fe1be61826fbce153591be01ecc26870368f6
Pratyushkr902 <00pratyush20@gmail.com>
2025-10-08 23:45:09 +0530

    Update server.js

--- frontend/server.js
+++ frontend/server.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on https://localhost:${PORT}`);
 });
 // server.js -> The CORRECT way
 
